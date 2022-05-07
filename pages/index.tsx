36d9a442e152f291f8642144e741022947586c28
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-05-07 09:07:09 +0200

    imported TimerContainer & TimerInput components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Header } from '../components/Header'
+import Header  from '../components/Header'
+import TimerContainer from '../components/TimerContainer'
+import TimerInput from '../components/TimerInput'
 import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
